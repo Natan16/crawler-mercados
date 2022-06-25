cc32ab45f9e6a86a7965e1354027b5d687a81897
Natan Lima Viana <natan.viana@buser.com.br>
2022-06-25 11:26:27 -0300

    Configura proxy

--- crawler_shibata.py
+++ crawler_shibata.py
@@ -1,4 +1,4 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from time import sleep
@@ -46,7 +46,13 @@ def constroi_dicionario_de_links(departamentos, deps_dict):
             link = a.get_attribute('href')
             cats_dict[str(a)] = link
 
-navegador = Chrome()
+
+# proxybroker serve --host 127.0.0.1 --port 8888 --types HTTP HTTPS --lvl High
+PROXY = 'http://localhost:8888'
+chrome_options = ChromeOptions()
+chrome_options.add_argument('--proxy-server=%s' % PROXY)
+navegador = Chrome(options=chrome_options)
+
 navegador.get(BASE_URL)
 escolher_loja()
 navegador.fullscreen_window()
