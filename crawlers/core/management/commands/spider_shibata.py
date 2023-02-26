ccfba4721de81a0ad5a7cf9d3e4d1b032bceda97
Natan Viana <natan.viana@buser.com.br>
2023-02-26 19:04:24 -0300

    Se prepara pra escrever spider do shibata

---
+++ crawlers/core/management/commands/spider_shibata.py
@@ -0,0 +1,12 @@
+# Request Headers
+# https://api.loja.shibata.com.br/v1/loja/classificacoes_mercadologicas/secoes/99/produtos/filial/1/centro_distribuicao/1/ativos?orderby=produto.descricao:asc
+# Request Headers
+# Accept: application/json
+# Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2aXBjb21tZXJjZSIsImF1ZCI6ImFwaS1hZG1pbiIsInN1YiI6IjZiYzQ4NjdlLWRjYTktMTFlOS04NzQyLTAyMGQ3OTM1OWNhMCIsInZpcGNvbW1lcmNlQ2xpZW50ZUlkIjpudWxsLCJpYXQiOjE2Nzc0NDgwNjcsInZlciI6MSwiY2xpZW50IjpudWxsLCJvcGVyYXRvciI6bnVsbCwib3JnIjoiMTYxIn0.RANSjc1q_mpotLABDE1Yr9oEETGEvv_jc-9xdos2JoMAJFTu7VMIBNkM_Sv8q7XblMpCfprDHFsUIR223ZnF0Q
+# Content-Type: application/json
+# OrganizationID: 161
+# Referer: https://www.loja.shibata.com.br/
+# sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"
+# sec-ch-ua-mobile: ?0
+# sec-ch-ua-platform: "Linux"
+# User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
