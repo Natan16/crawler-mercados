3b2d82ee643e88db8320bf7ecf12bdbe2bbe1279
Natan Viana <natan.viana@buser.com.br>
2023-03-10 14:44:31 -0300

    Garante que tá pegando só o último crawl de cada mercado

--- crawlers/core/service/produto_svc.py
+++ crawlers/core/service/produto_svc.py
@@ -8,10 +8,14 @@ CharField.register_lookup(Lower)
 # deve ter um jeito de otimizar a busca
 # vou usar o django qserializer do Iuri?
 # TODO: fazer a paginação
+# fazer o crawler de pelo menos mais 1 mercado
+# colocar adsense
 def search_produtos(search_term):
+    # se voltar vazio tenta tirando a última palavra ou usando icontains, como era antes
     produto_qs = Produto.objects.filter(nome__search=search_term)
+    
     crawl_qs = Crawl.objects.filter(
         created_at__gte=datetime.now() - timedelta(days=7)
-    )
-    produto_crawl_qs = ProdutoCrawl.objects.filter(produto__in=produto_qs, crawl__in=crawl_qs).select_related("crawl", "crawl__mercado", "produto").order_by("preco")  # pegar só o Crawl mais recente de cada mercado
+    ).distinct('mercado').order_by('mercado')
+    produto_crawl_qs = ProdutoCrawl.objects.filter(produto__in=produto_qs, crawl__in=crawl_qs).select_related("crawl", "crawl__mercado", "produto").order_by("preco")
     return produto_crawl_qs
