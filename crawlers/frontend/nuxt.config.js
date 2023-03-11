d727d075f07eeb2c5fc21c75604352e44646b835
Natan Viana <natan.viana@buser.com.br>
2023-03-11 15:26:31 -0300

    Coloca ads

--- crawlers/frontend/nuxt.config.js
+++ crawlers/frontend/nuxt.config.js
@@ -13,6 +13,14 @@ export default {
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
+    script: [
+      {
+        hid: 'adsense',
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1271835845256982',
+        crossorigin: 'anonymous',
+        defer: true
+      }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
