f14ba68445f76c53d0b0567d95e3985a77dd5847
Natan Viana <natan.viana@buser.com.br>
2023-03-16 20:28:10 -0300

    Só um crawl por mercado

--- crawlers/core/service/produto_svc.py
+++ crawlers/core/service/produto_svc.py
@@ -7,8 +7,6 @@ from functools import reduce
 
 CharField.register_lookup(Lower)
 
-# TODO: fazer a paginação
-# colocar adsense
 
 def search_produtos(search_term):
     words = search_term.split()
@@ -17,6 +15,14 @@ def search_produtos(search_term):
     produto_qs = Produto.objects.filter(query)
     crawl_qs = Crawl.objects.filter(
         created_at__gte=datetime.now() - timedelta(days=7)
-    ).distinct('mercado').order_by('mercado', "-id")
-    produto_crawl_qs = ProdutoCrawl.objects.filter(produto__in=produto_qs, crawl__in=crawl_qs).select_related("crawl", "crawl__mercado", "produto").order_by("preco")
+    ).order_by('created_at')
+    mercado_crawl_map = {}
+    for crawl in crawl_qs:
+        mercado_crawl_map[crawl.mercado.pk] = crawl
+    # tem que agrupar junto produtos iguais, e deixar próximo produtos parecidos só depois ordena por preço. Como fazer isso?
+    # a search boa é a chave pra um bom produto
+    # o peso igual é que é interessante ter ... não que não possa ser pesquisado
+    produto_crawl_qs = ProdutoCrawl.objects.filter(produto__in=produto_qs, crawl__in=mercado_crawl_map.values()).select_related("crawl", "crawl__mercado", "produto").order_by("preco")
+    # tem que priorizar correspondências exatas, só depois olha pro preço. Priorizar quando começa com o texto também
+    # se tem que começa com o texto ( exato ) ... não é bom por causa de Coca cola e Refrigerante Coca cola
     return produto_crawl_qs
