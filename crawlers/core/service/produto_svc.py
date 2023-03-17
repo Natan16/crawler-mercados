3e3957415a0778ee4031653a5ba40782b73c8d04
Natan Viana <natan.viana@buser.com.br>
2023-03-16 21:20:29 -0300

    Tira comentários

--- crawlers/core/service/produto_svc.py
+++ crawlers/core/service/produto_svc.py
@@ -19,10 +19,5 @@ def search_produtos(search_term):
     mercado_crawl_map = {}
     for crawl in crawl_qs:
         mercado_crawl_map[crawl.mercado.pk] = crawl
-    # tem que agrupar junto produtos iguais, e deixar próximo produtos parecidos só depois ordena por preço. Como fazer isso?
-    # a search boa é a chave pra um bom produto
-    # o peso igual é que é interessante ter ... não que não possa ser pesquisado
     produto_crawl_qs = ProdutoCrawl.objects.filter(produto__in=produto_qs, crawl__in=mercado_crawl_map.values()).select_related("crawl", "crawl__mercado", "produto").order_by("preco")
-    # tem que priorizar correspondências exatas, só depois olha pro preço. Priorizar quando começa com o texto também
-    # se tem que começa com o texto ( exato ) ... não é bom por causa de Coca cola e Refrigerante Coca cola
     return produto_crawl_qs
