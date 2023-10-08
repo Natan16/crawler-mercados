82c9faaf8341d8ac5eea289a32707afc4cad0ee5
Natan16 <natanvianat16@gmail.com>
2023-10-08 17:42:53 -0300

    Coloca snacks por toda parte

--- crawlers/frontend/pages/index.vue
+++ crawlers/frontend/pages/index.vue
@@ -178,14 +178,14 @@ export default {
       const newProdutos = this.produtos[idx]
       newProdutos.produto_crawl[idxItem].quantidade += 1
       Vue.set(this.produtos, idx, newProdutos)
-      // Snacks.show(this.$store, {text: `${this.produtos[idx].produto.nome} adicionado à lista`, timeout: 2000})
+      Snacks.show(this.$store, {text: `${newProdutos.produto_crawl[idxItem].produto_nome} adicionado à lista`, timeout: 2000})
     },
     removerItem (idx, idxItem) {
       this.$store.commit('lista/removeItem', this.produtos[idx].produto_crawl[idxItem])
       const newProdutos = this.produtos[idx]
       newProdutos.produto_crawl[idxItem].quantidade -= 1
       Vue.set(this.produtos, idx, newProdutos)
-      // Snacks.show(this.$store, {text: `${this.produtos[idx].produto.nome} removido da lista`, timeout: 2000})
+      Snacks.show(this.$store, {text: `${newProdutos.produto_crawl[idxItem].produto_nome} removido da lista`, timeout: 2000})
     },
     updateQuantidades () {
       const produtos = this.produtos
