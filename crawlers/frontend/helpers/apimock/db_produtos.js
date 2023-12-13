ed670999f66e8125af97192450fd7570a43cb7e9
Natan16 <natanvianat16@gmail.com>
2023-12-13 13:43:58 -0300

    Api mock para busca

---
+++ crawlers/frontend/helpers/apimock/db_produtos.js
@@ -0,0 +1,41 @@
+export const produtos = [
+      {
+        "mercado": {"unidade": "Shibata - Jardim Oriente", "rede": "SHIBATA"},
+        "produto_crawl": [
+            {
+                "id": 12,
+                "preco": "14.69",
+                "produto": {
+                    "id": 345,
+                    "nome": "Ketchup Heinz Picante - 397g",
+                },
+                "tags": ["acima da média"]
+            },
+            // # Ketchup Heinz - 1,033kg R$17.79
+            {
+                "id": 67,
+                "preco": "30.90",
+                "nome": "Ketchup Heinz Sachê 7g 192 Unidades",
+                "produto": {
+                    "id": 890,
+                    "nome": "Ketchup Heinz Picante - 397g",
+                },
+                "tags": ["mais em conta", "abaixo da média"]
+            },
+        ]
+    },
+    {
+      "mercado": {"unidade": "Carrefour - Jardim Serimbura", "rede": "CARREFOUR"},
+        "produto_crawl": [
+            {
+                "id": 24,
+                "preco": "14.69",
+                "produto": {
+                    "id": 345,
+                    "nome": "Ketchup Heinz Picante - 397g",
+                },
+                "tags": []
+            },
+        ]
+    }
+  ]
