036c9a668827d0f638c9f6c040a11e9523875fa0
Natan16 <natanvianat16@gmail.com>
2023-12-14 14:29:05 -0300

    Layout desktop ok

--- crawlers/frontend/pages/index.vue
+++ crawlers/frontend/pages/index.vue
@@ -31,7 +31,7 @@
           <p class="text-h8">{{mercadoResponse.mercado.unidade}} </p>
         </v-flex>
       </v-layout>
-      <v-slide-group
+      <v-slide-group v-if="mobile"
           selected-class="bg-success"
       >
         <v-slide-item
@@ -39,36 +39,23 @@
           v-for="(item, idxItem) in mercadoResponse.produto_crawl"
           :key="idxItem"
         >
-          <v-card class="d-flex flex-column" width="35vw" >
-            <v-chip style="border-radius: 0;" v-visible="item.tags.includes('mais em conta')" small color="green" text-color="white">MAIS EM CONTA</v-chip>
-            <v-card-title>
-              <v-row>
-                R$ {{ item.preco }}
-                <v-icon v-if="item.tags.includes('abaixo da média')" color="green">mdi-arrow-down</v-icon>
-                <v-icon v-else-if="item.tags.includes('acima da média')" color="red">mdi-arrow-up</v-icon>
-                <v-icon v-else color="yellow">mdi-minus</v-icon>
-              </v-row>
-            </v-card-title>
-            <v-card-text style="text-align: left;">
-              <v-row>
-                {{item.produto.nome}}
-              </v-row>
-            </v-card-text>
-            <v-spacer></v-spacer>
-            <v-card-actions class="pa-0">
-              <v-list-item>
-                <v-row
-                  justify="end"
-                >
-                  <v-btn v-visible="item.quantidade > 0" rounded x-small @click="removerItem(idx, idxItem)"><v-icon>mdi-minus</v-icon></v-btn>
-                  <span v-visible="item.quantidade > 0">{{item.quantidade}}</span>
-                  <v-btn rounded x-small @click="adicionarItem(idx, idxItem)"><v-icon>mdi-plus</v-icon></v-btn>
-                </v-row>
-              </v-list-item>
-            </v-card-actions>
-          </v-card>
+          <item :item="item"
+            @adicionar-item="adicionarItem(idx, idxItem)"
+            @remover-item="removerItem(idx, idxItem)"
+          />
         </v-slide-item>
       </v-slide-group>
+      <v-row wrap v-else class="mb-2">
+        <div
+          v-for="(item, idxItem) in mercadoResponse.produto_crawl"
+          :key="idxItem"
+        >
+          <item style="max-width: 20vw;" :item="item"
+            @adicionar-item="adicionarItem(idx, idxItem)"
+            @remover-item="removerItem(idx, idxItem)"
+          />
+        </div>
+      </v-row>
       <v-divider class="mt-2" />
     </div>
 
@@ -86,10 +73,12 @@ import Vue from 'vue'
 import api from '~api'
 import Snacks from '~/helpers/Snacks.js'
 import loading from '~/components/loading'
+import item from '~/components/item'
 
 export default {
   components: {
-    loading
+    loading,
+    item
   },
   data () {
     return {
